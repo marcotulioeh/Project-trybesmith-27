ca043c2f202f7fa0638e8216e7f29cf7debad375
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-09-14 17:58:06 -0300

    feat: adiciona um endpoint para o cadastro de produtos.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import ProductRouter from './routers/product.router';
 
 const app = express();
 
 app.use(express.json());
+
+app.get('/', (_req: Request, res: Response) => res.json({ message: 'Ok!' }));
+
+app.use('/products', ProductRouter);
 
 export default app;
