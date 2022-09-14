c7eead69af25498f7cdbce0bd70ae5c401388d61
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-09-14 17:58:48 -0300

    feat: adiciona um endpoint para a listagem de produtos.

--- src/routers/product.router.ts
+++ src/routers/product.router.ts
@@ -7,4 +7,6 @@ const productController = new ProductController();
 
 router.post('/', productController.create.bind(productController));
 
+router.get('/', productController.getAll.bind(productController));
+
 export default router;
